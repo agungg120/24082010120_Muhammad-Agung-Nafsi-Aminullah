494bef09be6275f8b3c81c9c410b482d109b455d
agungg120 <mldiamond999@gmail.com>
2024-09-15 10:31:31 +0700

    Add files via upload

---
+++ HelloWorldAgung.java
@@ -0,0 +1,13 @@
+package com.mycompany.helloworldagung;
+
+public class HelloWorldAgung {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("");
+        System.out.println("Aku Cinta Indonesia.");
+        System.out.println("");
+        System.out.println("Nama : Muhammad Agung Nafsi Aminullah");
+        System.out.println("NPM : 24082010120");
+    }
+}
